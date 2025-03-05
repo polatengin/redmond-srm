d12416e0c2263b9cb96d81f29ab80811defbf99d
polatengin <enpolat@microsoft.com>
2025-03-05 21:34:49 +0000

    creating initial entrypoint

---
+++ src/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "srm.h"
+
+void print_usage()
+{
+  printf("Usage: srm [options] <file/folder>\n");
+  printf("Options:\n");
+  printf("  --list           List deleted files\n");
+  printf("  --restore <file> Restore a deleted file\n");
+  printf("  --restore-all    Restore all deleted files\n");
+  printf("  --purge          Permanently delete all files in /recycle\n");
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc < 2)
+  {
+    print_usage();
+    return 1;
+  }
+
+  if (strcmp(argv[1], "--list") == 0)
+  {
+    srm_list();
+  }
+  else if (strcmp(argv[1], "--restore") == 0)
+  {
+    if (argc < 3)
+    {
+      fprintf(stderr, "Error: Specify a file to restore.\n");
+      return 1;
+    }
+    srm_restore(argv[2]);
+  }
+  else if (strcmp(argv[1], "--restore-all") == 0)
+  {
+    srm_restore_all();
+  }
+  else if (strcmp(argv[1], "--purge") == 0)
+  {
+    srm_purge();
+  }
+  else
+  {
+    srm_move_to_recycle(argv[1]);
+  }
+
+  return 0;
+}
